a8732f79956a94772b99925d7740764ac0fa2c86
Ryan Diercks <ryandiercks@hotmail.com>
2019-03-23 22:07:47 -0500

    simple node fix

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    fs.writeFile('hello-world.txt','Hello to this great world',(err) => {
+        if (err) throw err;
+        console.log('Saved');
+    })
+}
+
+const server = http.createServer(requestHandler);
+server.listen(port, (err)=>{
+    if (err) {
+        return console.log(`you have an error: ${err}`);
+    }
+    console.log(`server is listening on ${port}`);
+});
